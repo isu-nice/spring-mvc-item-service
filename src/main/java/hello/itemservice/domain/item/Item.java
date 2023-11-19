a019dea4a11a2e6e224753b0172f2461f5269ada
isu-nice <isu010614@naver.com>
2023-11-19 15:10:00 +0900

    feat: item 객체 생성

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,23 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price; // price가 null 인 경우도 있을 것이라고 가정하기 때문에 int 대신 Integer 사용
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
